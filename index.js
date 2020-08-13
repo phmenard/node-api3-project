3d10a013cb0adcddc54eecf633dacf84344764a9
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-08-13 01:32:03 -0400

    built index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code away!
+const express = require("express");
+
+const logger = require("./middleware/logger");
+
+const usersRouter = require("./users/userRouter")
+
+
+const server = express();
+const port = 4000;
+
+server.use(express.json());
+
+server.use(usersRouter);
+
+server.use(logger);
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+
+server.listen(port, () =>{
+    console.log(`Server listening at http://localhost:${port}`);
+
+})
+ 
